5772b65fc36e6bf137cddfd3ec9c24636fdfe603
tatangmemed-rgb <tatangmemed@gmail.com>
2026-09-01 15:08:18 +0700

    Delete app/src/main/java/com/medivet1437/aplikasitoko/ui/theme/Theme.kt

--- app/src/main/java/com/medivet1437/aplikasitoko/ui/theme/Theme.kt
+++
@@ -1,5 +0,0 @@
-package com.medivet1437.aplikasitoko.ui.theme
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-private val LightColors = lightColorScheme(primary=androidx.compose.ui.graphics.Color(0xFF159957), secondary=androidx.compose.ui.graphics.Color(0xFF1565C0))
-@Composable fun AppTheme(content:@Composable()->Unit){MaterialTheme(colorScheme=LightColors,content=content)}
